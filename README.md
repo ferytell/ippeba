a27da951d18110a2693281116256620ba6620a77
ferytell <darkstar2709@gmail.com>
2025-05-13 17:49:14 +0700

    test push

---
+++ README.md
@@ -0,0 +1,3 @@
+# IPPEBA
+
+testing!
---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+//import FlappishGame from "./FlappishGame";
+
+function App() {
+  return (
+    <div className="App">
+      <div>testings</div>
+    </div>
+  );
+}
+
+export default App;
