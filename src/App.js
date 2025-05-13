e55545420cfd5f23869775c62392467476b1f1f3
ferytell <darkstar2709@gmail.com>
2025-05-13 18:01:34 +0700

    test commits

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+//import FlappishGame from "./FlappishGame";
+
+function App() {
+  return (
+    <div className="App">
+      <div>test lalal</div>
+    </div>
+  );
+}
+
+export default App;
